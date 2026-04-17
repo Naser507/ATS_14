7081ad180f890a993f7cabec6a11abdf3303dfbf
Naser Abdullah <naser507@gmail.com>
2026-04-17 16:45:34 +0600

    Add GUI window and plugin-driven wxWidgets UI test integration

--- core/modules/ui_core/ui_core.cpp
+++ core/modules/ui_core/ui_core.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <wx/wx.h>
 
 extern "C" void init() {
-    std::cout << "UI Core Module Loaded\n";
+    std::cout << "UI Core Module Initializing GUI layer...\n";
+
+    wxMessageBox(
+        "UI Core Module Loaded",
+        "ATS_14 Plugin",
+        wxOK | wxICON_INFORMATION
+    );
 }
