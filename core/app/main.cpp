6e3beb7b00fc9ea94f0c89bc5293094d07036eb3
Naser Abdullah <naser507@gmail.com>
2026-04-17 16:36:10 +0600

    Introduce wxWidgets GUI shell with working desktop window and integrate with plugin loader

--- core/app/main.cpp
+++ core/app/main.cpp
@@ -1,11 +1,27 @@
+#include <wx/wx.h>
 #include <iostream>
 
 void load_module(const char* path);
 
-int main() {
-    std::cout << "ATS_14 Orchestrator Starting...\n";
+class MyApp : public wxApp {
+public:
+    bool OnInit() override;
+};
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame() : wxFrame(nullptr, wxID_ANY, "ATS_14") {
+        std::cout << "GUI Window Created\n";
+    }
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit() {
+    MyFrame* frame = new MyFrame();
+    frame->Show(true);
 
     load_module("./bin/libui_core.so");
 
-    return 0;
+    return true;
 }
